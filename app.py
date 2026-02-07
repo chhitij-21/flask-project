663829eaae1ff565b48e8f0b7dbd025a4dc0393b
chhitij-21 <chhitij.rai@hotmail.com>
2026-02-08 03:33:15 +0530

    Reset to Item ID only state

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+# MongoDB Connection
+client = MongoClient("mongodb://localhost:27017/")
+db = client["todo_db"]
+collection = db["items"]
 
 @app.route('/')
 def home():
     return render_template("todo.html")
 
+@app.route('/submittodoitem', methods=['POST'])
+def submittodoitem():
+
+    itemName = request.form.get("itemName")
+    itemDescription = request.form.get("itemDescription")
+
+    data = {
+        "itemName": itemName,
+        "itemDescription": itemDescription
+    }
+
+    collection.insert_one(data)
+
+    return jsonify({"message": "Item stored successfully"})
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
